c8e38ee7238c4c94a22f33920784b61310d5f07f
nollieBoi <54951811+nollieBoi@users.noreply.github.com>
2020-01-08 09:30:08 -0800

    second ya knaw man

---
+++ src/num.java
@@ -0,0 +1,24 @@
+public class num {
+    private int value;
+
+    public Num(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "" + value;
+    }
+
+    public Boolean equals(num otherNum){
+        return this.value == otherNum.value;
+    }
+}
